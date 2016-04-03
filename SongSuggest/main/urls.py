b7a3d5546f92a27a4ebba486920b3787e8a8ef2e
Bailey Anderson <baileyand03@gmail.com>
2016-04-03 02:21:10 -0400

    intermediate commit for branch switch

--- SongSuggest/main/urls.py
+++ SongSuggest/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
-    url(r'^$', views.main, name='main') #views.[something defined in views.py]
+    url(r'^$', views.main, name='main'), #views.[something defined in views.py]
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
 ]
--- SongSuggest/main/views.py
+++ SongSuggest/main/views.py
@@ -5,6 +5,17 @@ from django.http import HttpResponse
 #def index(request):
  #   return HttpResponse("Hello, world. You're at the polls index.")
 def main(request):
+	# username = request.POST['username']
+ #    password = request.POST['password']
+ #    user = authenticate(username=username, password=password)
+ #    if user is not None:
+ #        if user.is_active:
+ #            login(request, user)
+ #            # Redirect to a success page.
+ #        else:
+ #            # Return a 'disabled account' error message
+ #    else:
+ #        # Return an 'invalid login' error message.
 	return render(request, "main/main.html", {}) #searches in firstapp/templates/firstapp/page2.html
 
 # Create your views here.
